6b7ce3ab3539c82e3e447caa5077f90545545a6d
mases777 <mases@abv.bg>
2023-03-01 09:42:00 +0200

    Demo2

--- TU-Varna-Obuchenie/demo.py
+++ TU-Varna-Obuchenie/demo.py
@@ -1,7 +1,11 @@
 import turtle
-side = int(input("Length of size: "))
+#side = int(input("Length of size: "))
+colors = ['yellow','green','red','white','brown','pink']
 turtle.clear()
-for i in range(5):
-  turtle.forward(side)
-  turtle.left(72)
+turtle.bgcolor('blue')
+for i in range(200):
+  turtle.pencolor(colors[i % 6])
+  turtle.width(i / 100 + 1)
+  turtle.forward(i)
+  turtle.left(59)
 turtle.done()
