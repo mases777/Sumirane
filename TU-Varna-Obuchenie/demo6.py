4b4be559a4a97bd7e3c2339280f7b3aa0de24cf7
mases777 <mases@abv.bg>
2023-03-09 15:58:26 +0200

    Demo3b

---
+++ TU-Varna-Obuchenie/demo6.py
@@ -0,0 +1,90 @@
+import turtle
+# initialize variables
+
+turtle.clear()
+turtle.penup()
+turtle.setx(-160)
+turtle.sety(-100)
+turtle.left(180)
+turtle.bgcolor('violet')
+turtle.color('green')
+turtle.pensize(5)
+turtle.pendown()
+for count in range(5):
+  turtle.forward(10)
+  turtle.right(10)
+
+turtle.left(90)
+turtle.forward(20)
+for count2 in range(18):
+  turtle.forward(2)
+  turtle.right(10)
+
+turtle.forward(20)
+turtle.left(90)
+for count3 in range(4):
+  turtle.forward(10)
+  turtle.right(10)
+
+turtle.forward(90)
+for count4 in range(5):
+  turtle.forward(10)
+  turtle.right(10)
+
+turtle.left(90)
+turtle.forward(20)
+for count5 in range(18):
+  turtle.forward(2)
+  turtle.right(10)
+
+turtle.forward(20)
+turtle.left(90)
+for count6 in range(2):
+  turtle.forward(10)
+  turtle.right(10)
+
+turtle.left(90)
+for count7 in range(23):
+  turtle.forward(8)
+  turtle.right(10)
+
+turtle.left(100)
+for count8 in range(2):
+  turtle.forward(10)
+  turtle.right(10)
+
+turtle.left(90)
+turtle.forward(20)
+for count9 in range(18):
+  turtle.forward(2)
+  turtle.right(10)
+
+turtle.forward(20)
+turtle.left(90)
+for count10 in range(5):
+  turtle.forward(10)
+  turtle.right(10)
+
+turtle.forward(90)
+for count11 in range(5):
+  turtle.forward(10)
+  turtle.right(10)
+
+turtle.left(90)
+turtle.forward(20)
+for count12 in range(18):
+  turtle.forward(2)
+  turtle.right(10)
+
+turtle.forward(20)
+turtle.left(90)
+for count13 in range(4):
+  turtle.forward(10)
+  turtle.right(10)
+
+turtle.left(60)
+turtle.forward(45)
+turtle.right(120)
+turtle.forward(45)
+
+turtle.done()
